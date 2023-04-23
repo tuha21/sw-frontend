40b4de1df0cd35f650dd6ebcefc50c1e14d7d8e3
unknown <tuha1021@gmail.com>
2023-04-23 22:21:21 +0700

    siajsi

--- src/apis/baseApi.js
+++ src/apis/baseApi.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const newUrl = "sw-service-production-ae16.up.railway.app";
+
 const env = 1
 const PRODUCT_DOMAIN = env === 1 ? 'https://sw-service-production.up.railway.app' : 'http://localhost:8080'
 export const callApi = async (options) => {
